2303a24a64f7011b19a8b869717ab904da59b22d
mohammed rishad <mohammedrishad17@gmail.com>
2022-09-05 17:04:21 +0400

    textarea added

--- header.php
+++ header.php
@@ -2,6 +2,10 @@
 ob_start();
 session_start();
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
 if (defined('RESTRICTED')) {
     if (!isset($_SESSION['id'])) {
         header('Location: index.php');
